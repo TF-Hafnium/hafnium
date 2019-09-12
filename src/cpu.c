eed724bf8b748681a14fe3fbb60401e1b3ebeef1
Andrew Scull <ascull@google.com>
2019-09-12 09:57:19 +0100

    Align stacks to page boundaries.
    Page alignment supersedes the previous alignment that was a requirement
    of aarch64. It also ensures cache lines don't contain data other than
    the stack which could be accessed from other cores.
    
    Change-Id: I3a9eca651e52062c93d8e6b3278e158a65f1c1f5

--- src/cpu.c
+++ src/cpu.c
@@ -31,8 +31,20 @@
 
 #define STACK_SIZE PAGE_SIZE
 
-/* The stack to be used by the CPUs. */
-alignas(2 * sizeof(uintreg_t)) static char callstacks[MAX_CPUS][STACK_SIZE];
+/**
+ * The stacks to be used by the CPUs.
+ *
+ * Align to page boundaries to ensure that cache lines are not shared between a
+ * CPU's stack and data that can be accessed from other CPUs. If this did
+ * happen, there may be coherency problems when the stack is being used before
+ * caching is enabled.
+ */
+alignas(PAGE_SIZE) static char callstacks[MAX_CPUS][STACK_SIZE];
+
+/* NOLINTNEXTLINE(misc-redundant-expression) */
+static_assert((STACK_SIZE % PAGE_SIZE) == 0, "Keep each stack page aligned.");
+static_assert((PAGE_SIZE % STACK_ALIGN) == 0,
+	      "Page alignment is too weak for the stack.");
 
 /**
  * Internal buffer used to store SPCI messages from a VM Tx. Its usage prevents
