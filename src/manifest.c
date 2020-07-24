TF-Hafnium/hafnium

commit e4efcc3ffb47ed4d6104b264a11165db42cafcc9

Add support for FFA_PARTITION_INFO_GET
Add support for FFA_PARTITION_INFO_GET, which returns
information on the partitions instantiated in the system.

Signed-off-by: Fuad Tabba <tabba@google.com>

Change-Id: I93070fe841b4b19c596645246203dbba14eddb12

src/manifest.c
@@ -317,7 +317,7 @@ static enum manifest_return_code parse_ffa_manifest(struct fdt *fdt,
 
 	while (uint32list_has_next(&uuid) && i < 4) {
 		TRY(uint32list_get_next(&uuid, &uuid_word));
-		vm->sp.uuid[i] = uuid_word;
+		vm->sp.uuid.uuid[i] = uuid_word;
 		i++;
 	}
 	dlog_verbose("  SP UUID %#x-%x-%x_%x\n", vm->sp.uuid[0], vm->sp.uuid[1],

inc/hf/api.h
@@ -48,6 +48,8 @@ struct ffa_value api_ffa_rxtx_map(ipaddr_t send, ipaddr_t recv,
 				  struct vcpu **next);
 void api_yield(struct vcpu *current, struct vcpu **next);
 struct ffa_value api_ffa_version(uint32_t requested_version);
+struct ffa_value api_ffa_partition_info_get(struct vcpu *current,
+					    const struct ffa_uuid *uuid);
 struct ffa_value api_ffa_id_get(const struct vcpu *current);
 struct ffa_value api_ffa_features(uint32_t function_id);
 struct ffa_value api_ffa_run(ffa_vm_id_t vm_id, ffa_vcpu_index_t vcpu_idx,

inc/hf/manifest.h
@@ -47,7 +47,7 @@ struct sp_manifest {
 	/** PSA-FF-A expected version - mandatory */
 	uint32_t ffa_version;
 	/** UUID - mandatory */
-	uint32_t uuid[4];
+	struct ffa_uuid uuid;
 	/** Partition id - optional */
 	ffa_vm_id_t id;
 	/** Aux ids for mem transactions - optional */

inc/hf/vm.h
@@ -100,6 +100,7 @@ struct smc_whitelist {
 
 struct vm {
 	ffa_vm_id_t id;
+	struct ffa_uuid uuid;
 	struct smc_whitelist smc_whitelist;
 
 	/** See api.c for the partial ordering on locks. */
@@ -140,6 +141,7 @@ bool vm_init_next(ffa_vcpu_count_t vcpu_count, struct mpool *ppool,
 		  struct vm **new_vm);
 ffa_vm_count_t vm_get_count(void);
 struct vm *vm_find(ffa_vm_id_t id);
+struct vm *vm_find_index(uint16_t index);
 struct vm_locked vm_lock(struct vm *vm);
 struct two_vm_locked vm_lock_both(struct vm *vm1, struct vm *vm2);
 void vm_unlock(struct vm_locked *locked);

inc/vmapi/hf/call.h
@@ -28,6 +28,31 @@ static inline struct ffa_value ffa_id_get(void)
 }
 
 /**
+ * Requests information for partitions instantiated in the system. The
+ * information is returned in the RX buffer of the caller as an array of
+ * partition information descriptors (struct ffa_partition_info).
+ *
+ * A Null UUID (UUID that is all zeros) returns information for all partitions,
+ * whereas a non-Null UUID returns information only for partitions that match.
+ *
+ * Returns:
+ *  - FFA_SUCCESS on success. The count of partition information descriptors
+ *    populated in the RX buffer is returned in arg2 (register w2).
+ *  - FFA_BUSY if the caller's RX buffer is not free.
+ *  - FFA_NO_MEMORY if the results do not fit in the callers RX buffer.
+ *  - FFA_INVALID_PARAMETERS for an unrecognized UUID.
+ */
+static inline struct ffa_value ffa_partition_info_get(
+	const struct ffa_uuid *uuid)
+{
+	return ffa_call((struct ffa_value){.func = FFA_PARTITION_INFO_GET_32,
+					   .arg1 = uuid->uuid[0],
+					   .arg2 = uuid->uuid[1],
+					   .arg3 = uuid->uuid[2],
+					   .arg4 = uuid->uuid[3]});
+}
+
+/**
  * Returns the VM's own ID.
  */
 static inline ffa_vm_id_t hf_vm_get_id(void)

inc/vmapi/hf/ffa.h
@@ -290,6 +290,71 @@ static inline ffa_vm_id_t ffa_frag_sender(struct ffa_value args)
 }
 
 /**
+ * Holds the UUID in a struct that is mappable directly to the SMCC calling
+ * convention, which is used for FF-A calls.
+ *
+ * Refer to table 84 of the FF-A 1.0 EAC specification as well as section 5.3
+ * of the SMCC Spec 1.2.
+ */
+struct ffa_uuid {
+	uint32_t uuid[4];
+};
+
+static inline void ffa_uuid_init(uint32_t w0, uint32_t w1, uint32_t w2,
+				 uint32_t w3, struct ffa_uuid *uuid)
+{
+	uuid->uuid[0] = w0;
+	uuid->uuid[1] = w1;
+	uuid->uuid[2] = w2;
+	uuid->uuid[3] = w3;
+}
+
+static inline bool ffa_uuid_equal(const struct ffa_uuid *uuid1,
+				  const struct ffa_uuid *uuid2)
+{
+	return (uuid1->uuid[0] == uuid2->uuid[0]) &&
+	       (uuid1->uuid[1] == uuid2->uuid[1]) &&
+	       (uuid1->uuid[2] == uuid2->uuid[2]) &&
+	       (uuid1->uuid[3] == uuid2->uuid[3]);
+}
+
+static inline bool ffa_uuid_is_null(const struct ffa_uuid *uuid)
+{
+	return (uuid->uuid[0] == 0) && (uuid->uuid[1] == 0) &&
+	       (uuid->uuid[2] == 0) && (uuid->uuid[3] == 0);
+}
+
+/**
+ * Flags to determine the partition properties, as required by
+ * FFA_PARTITION_INFO_GET.
+ *
+ * The values of the flags are specified in table 82 of the FF-A 1.0 EAC
+ * specification, "Partition information descriptor, partition properties".
+ */
+typedef uint32_t ffa_partition_properties_t;
+
+/** Partition property: partition supports receipt of direct requests. */
+#define FFA_PARTITION_DIRECT_RECV 0x1
+
+/** Partition property: partition can send direct requests. */
+#define FFA_PARTITION_DIRECT_SEND 0x2
+
+/** Partition property: partition can send and receive indirect messages. */
+#define FFA_PARTITION_INDIRECT_MSG 0x4
+
+/**
+ * Holds information returned for each partition by the FFA_PARTITION_INFO_GET
+ * interface.
+ * This corresponds to table 82 of the FF-A 1.0 EAC specification, "Partition
+ * information descriptor".
+ */
+struct ffa_partition_info {
+	ffa_vm_id_t vm_id;
+	ffa_vcpu_count_t vcpu_count;
+	ffa_partition_properties_t properties;
+};
+
+/**
  * A set of contiguous pages which is part of a memory region. This corresponds
  * to table 40 of the FF-A 1.0 EAC specification, "Constituent memory region
  * descriptor".

src/api.c
@@ -26,6 +26,10 @@
 #include "vmapi/hf/call.h"
 #include "vmapi/hf/ffa.h"
 
+static_assert(sizeof(struct ffa_partition_info) == 8,
+	      "Partition information descriptor size doesn't match the one in "
+	      "the FF-A 1.0 EAC specification, Table 82.");
+
 /*
  * To eliminate the risk of deadlocks, we define a partial order for the
  * acquisition of locks held concurrently by the same physical CPU. Our current
@@ -115,6 +119,35 @@ static struct vcpu *api_switch_to_primary(struct vcpu *current,
 	sl_unlock(&current->lock);
 
 	return next;
+}
+
+/**
+ * Checks whether the given `to` VM's mailbox is currently busy, and optionally
+ * registers the `from` VM to be notified when it becomes available.
+ */
+static bool msg_receiver_busy(struct vm_locked to, struct vm *from, bool notify)
+{
+	if (to.vm->mailbox.state != MAILBOX_STATE_EMPTY ||
+	    to.vm->mailbox.recv == NULL) {
+		/*
+		 * Fail if the receiver isn't currently ready to receive data,
+		 * setting up for notification if requested.
+		 */
+		if (notify) {
+			struct wait_entry *entry =
+				vm_get_wait_entry(from, to.vm->id);
+
+			/* Append waiter only if it's not there yet. */
+			if (list_empty(&entry->wait_links)) {
+				list_append(&to.vm->mailbox.waiter_list,
+					    &entry->wait_links);
+			}
+		}
+
+		return true;
+	}
+
+	return false;
 }
 
 /**
@@ -221,6 +254,82 @@ struct vcpu *api_abort(struct vcpu *current)
 	/* TODO: free resources once all vCPUs abort. */
 
 	return api_switch_to_primary(current, ret, VCPU_STATE_ABORTED);
+}
+
+struct ffa_value api_ffa_partition_info_get(struct vcpu *current,
+					    const struct ffa_uuid *uuid)
+{
+	struct vm *current_vm = current->vm;
+	struct vm_locked current_vm_locked;
+	ffa_vm_count_t vm_count = 0;
+	bool uuid_is_null = ffa_uuid_is_null(uuid);
+	struct ffa_value ret;
+	uint32_t size;
+	struct ffa_partition_info partitions[MAX_VMS];
+
+	/*
+	 * Iterate through the VMs to find the ones with a matching UUID.
+	 * A Null UUID retrieves information for all VMs.
+	 */
+	for (uint16_t index = 0; index < vm_get_count(); ++index) {
+		const struct vm *vm = vm_find_index(index);
+
+		if (uuid_is_null || ffa_uuid_equal(uuid, &vm->uuid)) {
+			partitions[vm_count].vm_id = vm->id;
+			partitions[vm_count].vcpu_count = vm->vcpu_count;
+
+			/* Hafnium only supports indirect messaging. */
+			partitions[vm_count].properties =
+				FFA_PARTITION_INDIRECT_MSG;
+
+			++vm_count;
+		}
+	}
+
+	/* Unrecognized UUID: does not match any of the VMs and is not Null. */
+	if (vm_count == 0) {
+		return ffa_error(FFA_INVALID_PARAMETERS);
+	}
+
+	size = vm_count * sizeof(partitions[0]);
+	if (size > FFA_MSG_PAYLOAD_MAX) {
+		dlog_error(
+			"Partition information does not fit in the VM's RX "
+			"buffer.\n");
+		return ffa_error(FFA_NO_MEMORY);
+	}
+
+	/*
+	 * Partition information is returned in the VM's RX buffer, which is why
+	 * the lock is needed.
+	 */
+	current_vm_locked = vm_lock(current_vm);
+
+	if (msg_receiver_busy(current_vm_locked, NULL, false)) {
+		/*
+		 * Can't retrieve memory information if the mailbox is not
+		 * available.
+		 */
+		dlog_verbose("RX buffer not ready.\n");
+		ret = ffa_error(FFA_BUSY);
+		goto out_unlock;
+	}
+
+	/* Populate the VM's RX buffer with the partition information. */
+	memcpy_s(current_vm->mailbox.recv, FFA_MSG_PAYLOAD_MAX, partitions,
+		 size);
+	current_vm->mailbox.recv_size = size;
+	current_vm->mailbox.recv_sender = HF_HYPERVISOR_VM_ID;
+	current_vm->mailbox.recv_func = FFA_PARTITION_INFO_GET_32;
+	current_vm->mailbox.state = MAILBOX_STATE_READ;
+
+	/* Return the count of partition information descriptors in w2. */
+	ret = (struct ffa_value){.func = FFA_SUCCESS_32, .arg2 = vm_count};
+
+out_unlock:
+	vm_unlock(&current_vm_locked);
+
+	return ret;
 }
 
 /**
@@ -870,35 +979,6 @@ exit:
 }
 
 /**
- * Checks whether the given `to` VM's mailbox is currently busy, and optionally
- * registers the `from` VM to be notified when it becomes available.
- */
-static bool msg_receiver_busy(struct vm_locked to, struct vm *from, bool notify)
-{
-	if (to.vm->mailbox.state != MAILBOX_STATE_EMPTY ||
-	    to.vm->mailbox.recv == NULL) {
-		/*
-		 * Fail if the receiver isn't currently ready to receive data,
-		 * setting up for notification if requested.
-		 */
-		if (notify) {
-			struct wait_entry *entry =
-				vm_get_wait_entry(from, to.vm->id);
-
-			/* Append waiter only if it's not there yet. */
-			if (list_empty(&entry->wait_links)) {
-				list_append(&to.vm->mailbox.waiter_list,
-					    &entry->wait_links);
-			}
-		}
-
-		return true;
-	}
-
-	return false;
-}
-
-/**
  * Notifies the `to` VM about the message currently in its mailbox, possibly
  * with the help of the primary VM.
  */
@@ -1428,6 +1508,7 @@ struct ffa_value api_ffa_features(uint32_t function_id)
 	case FFA_FEATURES_32:
 	case FFA_RX_RELEASE_32:
 	case FFA_RXTX_MAP_64:
+	case FFA_PARTITION_INFO_GET_32:
 	case FFA_ID_GET_32:
 	case FFA_MSG_POLL_32:
 	case FFA_MSG_WAIT_32:

src/arch/aarch64/hypervisor/handler.c
@@ -317,6 +317,14 @@ static bool ffa_handler(struct ffa_value *args, struct vcpu **next)
 	case FFA_VERSION_32:
 		*args = api_ffa_version(args->arg1);
 		return true;
+	case FFA_PARTITION_INFO_GET_32: {
+		struct ffa_uuid uuid;
+
+		ffa_uuid_init(args->arg1, args->arg2, args->arg3, args->arg4,
+			      &uuid);
+		*args = api_ffa_partition_info_get(current(), &uuid);
+		return true;
+	}
 	case FFA_ID_GET_32:
 		*args = api_ffa_id_get(current());
 		return true;

commit 952401ea4e98ebb2ba4c3ae7f19e14cd6c7b47df

Fix bug when printing UUID in verbose logging
Fix bug introduced in e4efcc3ffb47ed4d6104b264a11165db42cafcc9,
which causes verbose builds to fail.

Signed-off-by: Fuad Tabba <tabba@google.com>
Change-Id: I82d133bbd49ecb294c56f0b8d4a318450d88e671

src/manifest.c
@@ -320,8 +320,9 @@ static enum manifest_return_code parse_ffa_manifest(struct fdt *fdt,
 		vm->sp.uuid.uuid[i] = uuid_word;
 		i++;
 	}
-	dlog_verbose("  SP UUID %#x-%x-%x_%x\n", vm->sp.uuid[0], vm->sp.uuid[1],
-		     vm->sp.uuid[2], vm->sp.uuid[3]);
+	dlog_verbose("  SP UUID %#x-%x-%x_%x\n", vm->sp.uuid.uuid[0],
+		     vm->sp.uuid.uuid[1], vm->sp.uuid.uuid[2],
+		     vm->sp.uuid.uuid[3]);
 
 	TRY(read_uint16(&root, "execution-ctx-count",
 			&vm->sp.execution_ctx_count));