0c99aeca1c4f7a7c1138ac6aa8c13caa115bfa9d
Usama Arif <usama.arif@arm.com>
2021-03-15 14:15:18 +0000

    pl011: add UART initialization code
    This sets the IBRD and FBRD UART registers according
    to the specified UART clock and Baud rate.
    
    Signed-off-by: Usama Arif <usama.arif@arm.com>
    Change-Id: I44c83684b73539e3c0ab791b7b45164f7b618efa

--- src/arch/aarch64/pl011/pl011.c
+++ src/arch/aarch64/pl011/pl011.c
@@ -12,20 +12,75 @@
 #include "hf/plat/console.h"
 
 /* UART Data Register. */
-#define UARTDR IO32_C(PL011_BASE + 0x0)
+#define UART_DR IO32_C(PL011_BASE + 0x0)
+
+/* Receive Status or Error Clear Register. */
+#define UART_RSR_ECR IO32_C(PL011_BASE + 0x4)
 
 /* UART Flag Register. */
-#define UARTFR IO32_C(PL011_BASE + 0x018)
+#define UART_FR IO32_C(PL011_BASE + 0x018)
+
+/* UART IBRD (Integer Baudrate) Register. */
+#define UART_IBRD IO32_C(PL011_BASE + 0x024)
+
+/* UART FBRD (Fractional Baudrate) Register. */
+#define UART_FBRD IO32_C(PL011_BASE + 0x028)
+
+/* UART Line Control Register Register. */
+#define UART_LCR_H IO32_C(PL011_BASE + 0x02C)
+
+/* UART Control Register. */
+#define UART_CR IO32_C(PL011_BASE + 0x030)
+
+/* UART Interrupt Mask Set/Clear Register. */
+#define UART_IMSC IO32_C(PL011_BASE + 0x038)
 
 /* UART Flag Register bit: transmit fifo is full. */
-#define UARTFR_TXFF (1 << 5)
+#define UART_FR_TXFF (1 << 5)
 
 /* UART Flag Register bit: UART is busy. */
-#define UARTFR_BUSY (1 << 3)
+#define UART_FR_BUSY (1 << 3)
+
+/* UART transmit/receive line register bits. */
+#define UART_LCRH_WLEN_8 (3 << 5)
+
+/* UART control register bits. */
+#define UART_CR_RXE (1 << 9)
+#define UART_CR_TXE (1 << 8)
+#define UART_CR_UARTEN (1 << 0)
+
+#define UART_IMSC_RTIM (1 << 6)
+#define UART_IMSC_RXIM (1 << 4)
 
 void plat_console_init(void)
 {
-	/* No hardware initialisation required. */
+	/*
+	 * If pl011 clock frequency is 0 or not specified, then don't set
+	 * baudrate as it can't be calculated without clock frequency.
+	 * Assumption is that the default rate is fine for the system.
+	 */
+#if PL011_CLOCK != 0
+
+	unsigned int quotient = PL011_CLOCK * 4 / PL011_BAUDRATE;
+
+	/* Disable everything */
+	io_write32(UART_CR, 0);
+	/* Clear all errors */
+	io_write32(UART_RSR_ECR, 0);
+
+	/* Set baud rate */
+	io_write32(UART_FBRD, quotient & 0x3f);
+	io_write32(UART_IBRD, quotient >> 6);
+
+	/* Configure TX to 8 bits, 1 stop bit, no parity, fifo disabled. */
+	io_write32(UART_LCR_H, UART_LCRH_WLEN_8);
+
+	/* Enable interrupts for receive and receive timeout */
+	io_write32(UART_IMSC, UART_IMSC_RXIM | UART_IMSC_RTIM);
+
+	/* Enable UART and RX/TX */
+	io_write32(UART_CR, UART_CR_UARTEN | UART_CR_TXE | UART_CR_RXE);
+#endif
 }
 
 void plat_console_mm_init(struct mm_stage1_locked stage1_locked,
@@ -45,17 +100,17 @@ void plat_console_putchar(char c)
 	}
 
 	/* Wait until there is room in the tx buffer. */
-	while (io_read32(UARTFR) & UARTFR_TXFF) {
+	while (io_read32(UART_FR) & UART_FR_TXFF) {
 		/* do nothing */
 	}
 
 	/* Write the character out, force memory access ordering. */
 	memory_ordering_barrier();
-	io_write32(UARTDR, c);
+	io_write32(UART_DR, c);
 	memory_ordering_barrier();
 
 	/* Wait until the UART is no longer busy. */
-	while (io_read32_mb(UARTFR) & UARTFR_BUSY) {
+	while (io_read32_mb(UART_FR) & UART_FR_BUSY) {
 		/* do nothing */
 	}
 }
@@ -63,8 +118,8 @@ void plat_console_putchar(char c)
 char plat_console_getchar(void)
 {
 	/* Wait until the UART is no longer busy. */
-	while (io_read32_mb(UARTFR) & UARTFR_BUSY) {
+	while (io_read32_mb(UART_FR) & UART_FR_BUSY) {
 		/* do nothing */
 	}
-	return (char)(io_read32(UARTDR) & 0xFF);
+	return (char)(io_read32(UART_DR) & 0xFF);
 }
