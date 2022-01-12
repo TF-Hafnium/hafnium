a6c97669e0947a3d7076c7269d614f5093dd69f2
J-Alves <joao.alves@arm.com>
2022-01-12 10:24:10 +0000

    test: invoke vhe tests in test_spmc.sh
    Change test_spmc.sh to comply with refactor from previous patch, to
    fetch the build targets for VHE support testing.
    
    Change-Id: Id230fd43d6dcd048183c8922bc95a509c665f5ab
    Signed-off-by: J-Alves <joao.alves@arm.com>

--- kokoro/test_spmc.sh
+++ kokoro/test_spmc.sh
@@ -10,48 +10,26 @@
 KOKORO_DIR="$(dirname "$0")"
 source $KOKORO_DIR/test_common.sh
 
-USE_VHE=false
-EL0_TEST_ONLY=false
-
-while test $# -gt 0
-do
-  case "$1" in
-    --vhe) USE_VHE=true
-      ;;
-    --el0) EL0_TEST_ONLY=true
-      ;;
-    *) echo "Unexpected argument $1"
-      exit 1
-      ;;
-  esac
-  shift
-done
-
 HFTEST=(${TIMEOUT[@]} 300s ./test/hftest/hftest.py)
-if [ $USE_VHE == true ]
-then
-  SPMC_PATH="$OUT/secure_aem_v8a_fvp_vhe_clang"
-  HYPERVISOR_PATH="$OUT/aem_v8a_fvp_vhe_clang"
-  HFTEST+=(--out_partitions "$OUT/secure_aem_v8a_fvp_vhe_vm_clang")
-else
-  SPMC_PATH="$OUT/secure_aem_v8a_fvp_clang"
-  HYPERVISOR_PATH="$OUT/aem_v8a_fvp_clang"
-  HFTEST+=(--out_partitions "$OUT/secure_aem_v8a_fvp_vm_clang")
-fi
 
+SPMC_PATH_VHE="$OUT/secure_aem_v8a_fvp_vhe_clang"
+HYPERVISOR_PATH_VHE="$OUT/aem_v8a_fvp_vhe_clang"
+HFTEST_VHE="${HFTEST[@]} --out_partitions $OUT/secure_aem_v8a_fvp_vhe_vm_clang"
+
+SPMC_PATH="$OUT/secure_aem_v8a_fvp_clang"
+HYPERVISOR_PATH="$OUT/aem_v8a_fvp_clang"
+HFTEST+=(--out_partitions "$OUT/secure_aem_v8a_fvp_vm_clang")
+
+HFTEST_VHE+=(--log "$LOG_DIR_BASE")
 HFTEST+=(--log "$LOG_DIR_BASE")
+
+HFTEST_VHE+=(--spmc "$SPMC_PATH_VHE/hafnium.bin" --driver=fvp)
 HFTEST+=(--spmc "$SPMC_PATH/hafnium.bin" --driver=fvp)
 
-if [ $EL0_TEST_ONLY == false ]
-then
-  ${HFTEST[@]} --partitions_json test/vmapi/ffa_secure_partition_only/ffa_secure_partition_only_test.json
+${HFTEST[@]} --partitions_json test/vmapi/ffa_secure_partition_only/ffa_secure_partition_only_test.json
 
-  ${HFTEST[@]} --hypervisor "$HYPERVISOR_PATH/hafnium.bin" \
-               --partitions_json test/vmapi/ffa_secure_partitions/ffa_both_world_partitions_test.json
-fi
+${HFTEST[@]} --hypervisor "$HYPERVISOR_PATH/hafnium.bin" \
+             --partitions_json test/vmapi/ffa_secure_partitions/ffa_both_world_partitions_test.json
 
-if [ $USE_VHE == true ]
-then
-  ${HFTEST[@]} --hypervisor "$HYPERVISOR_PATH/hafnium.bin" \
-    --partitions_json test/vmapi/el0_partitions/secure_partitions/ffa_both_world_partitions_test.json
-fi
+${HFTEST_VHE[@]} --hypervisor "$HYPERVISOR_PATH/hafnium.bin" \
+                 --partitions_json test/vmapi/ffa_secure_partitions/ffa_both_world_partitions_vhe_test.json
