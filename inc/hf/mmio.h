f9bc7ce9b09978f80a3398c80394eba1902db335
Madhukar Pappireddy <madhukar.pappireddy@arm.com>
2023-01-06 15:19:22 -0600

    feat(mmio): introduce helpers for MMIO access
    These helpers are useful in drivers for working with memory mapped
    peripheral devices such as SMMUv3, Trusted Watchdog timer, etc.
    
    Signed-off-by: Madhukar Pappireddy <madhukar.pappireddy@arm.com>
    Change-Id: I374166b8c183c68e18449c1a6b20ed0d14561518

---
+++ inc/hf/mmio.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2023 The Hafnium Authors.
+ *
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/BSD-3-Clause.
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+#include "hf/io.h"
+
+static inline uint32_t mmio_read32(void *addr)
+{
+	return io_read32(IO32_C((uintpaddr_t)addr));
+}
+
+static inline uint32_t mmio_read32_offset(void *addr, uint32_t offset)
+{
+	return io_read32(io32_c((uintpaddr_t)addr, offset));
+}
+
+static inline void mmio_write32(void *addr, uint32_t data)
+{
+	io_write32(IO32_C((uintpaddr_t)addr), data);
+}
+
+static inline void mmio_write32_offset(void *addr, uint32_t offset,
+				       uint32_t data)
+{
+	io_write32(io32_c((uintpaddr_t)addr, offset), data);
+}
+
+static inline void mmio_write64(void *addr, uint64_t data)
+{
+	io_write64(IO64_C((uintpaddr_t)addr), data);
+}
+
+static inline void mmio_write64_offset(void *addr, uint32_t offset,
+				       uint64_t data)
+{
+	io_write64(io64_c((uintpaddr_t)addr, offset), data);
+}
--- src/arch/aarch64/arm_smmuv3/arm_smmuv3.h
+++ src/arch/aarch64/arm_smmuv3/arm_smmuv3.h
@@ -6,6 +6,7 @@
  * https://opensource.org/licenses/BSD-3-Clause.
  */
 
+#include "hf/mmio.h"
 #include "hf/plat/iommu.h"
 
 #define EXTRACT(data, shift, mask) (((data) >> (shift)) & (mask))
@@ -327,37 +328,3 @@ struct smmuv3_driver {
 	struct smmuv3_queue evt_queue;
 	struct smmuv3_stream_table_config strtab_cfg;
 };
-
-#include "hf/io.h"
-
-static inline uint32_t mmio_read32(void *addr)
-{
-	return io_read32(IO32_C((uintpaddr_t)addr));
-}
-
-static inline uint32_t mmio_read32_offset(void *addr, uint32_t offset)
-{
-	return io_read32(io32_c((uintpaddr_t)addr, offset));
-}
-
-static inline void mmio_write32(void *addr, uint32_t data)
-{
-	io_write32(IO32_C((uintpaddr_t)addr), data);
-}
-
-static inline void mmio_write32_offset(void *addr, uint32_t offset,
-				       uint32_t data)
-{
-	io_write32(io32_c((uintpaddr_t)addr, offset), data);
-}
-
-static inline void mmio_write64(void *addr, uint64_t data)
-{
-	io_write64(IO64_C((uintpaddr_t)addr), data);
-}
-
-static inline void mmio_write64_offset(void *addr, uint32_t offset,
-				       uint64_t data)
-{
-	io_write64(io64_c((uintpaddr_t)addr, offset), data);
-}
