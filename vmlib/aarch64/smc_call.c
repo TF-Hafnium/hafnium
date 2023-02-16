9ad2e28ed858bb95f8d389bd75624df15baa46eb
Shruti Gupta <shruti.gupta@arm.com>
2023-02-16 12:52:35 +0000

    test(el3-spmc): add infrastructure for EL3 SPMC tests
    * Add infrastructure for EL3 SPMC managed SP to call
      FFA ABIs through smc.
    * Add prebuilt TF-A binaries with SPMC at EL3,
      TSP as SP and TSP partition manifest dtb.
    
    Signed-off-by: Shruti Gupta <shruti.gupta@arm.com>
    Signed-off-by: Karl Meakin <karl.meakin@arm.com>
    Change-Id: I699d0a14f4d6c88f13c41eaa9af30cbdd2e0b41c

---
+++ vmlib/aarch64/smc_call.c
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2023 The Hafnium Authors.
+ *
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/BSD-3-Clause.
+ */
+
+#include "hf/call.h"
+#include "hf/ffa.h"
+#include "hf/types.h"
+
+int64_t hf_call(uint64_t arg0, uint64_t arg1, uint64_t arg2, uint64_t arg3)
+{
+	(void)arg0;
+	(void)arg1;
+	(void)arg2;
+	(void)arg3;
+
+	/* hafnium is absent, so return an error. */
+	return -1;
+}
+
+struct ffa_value ffa_call(struct ffa_value args)
+{
+	register uint64_t r0 __asm__("x0") = args.func;
+	register uint64_t r1 __asm__("x1") = args.arg1;
+	register uint64_t r2 __asm__("x2") = args.arg2;
+	register uint64_t r3 __asm__("x3") = args.arg3;
+	register uint64_t r4 __asm__("x4") = args.arg4;
+	register uint64_t r5 __asm__("x5") = args.arg5;
+	register uint64_t r6 __asm__("x6") = args.arg6;
+	register uint64_t r7 __asm__("x7") = args.arg7;
+
+	__asm__ volatile(
+		"smc #0"
+		: /* Output registers, also used as inputs ('+' constraint). */
+		"+r"(r0), "+r"(r1), "+r"(r2), "+r"(r3), "+r"(r4), "+r"(r5),
+		"+r"(r6), "+r"(r7));
+
+	return (struct ffa_value){.func = r0,
+				  .arg1 = r1,
+				  .arg2 = r2,
+				  .arg3 = r3,
+				  .arg4 = r4,
+				  .arg5 = r5,
+				  .arg6 = r6,
+				  .arg7 = r7};
+}
