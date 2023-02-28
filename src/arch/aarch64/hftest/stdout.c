fcfa2d074f1129ef0b9b7070594727d261335494
Kathleen Capella <kathleen.capella@arm.com>
2023-02-28 18:34:39 -0500

    feat(hftest): call ffa_console_log with many characters
    Optimize performance in hftest logging by buffering characters
    to stdout and using full character capacity of ffa_console_log_64
    instead of requesting one character be printed at a time.
    
    Signed-off-by: Kathleen Capella <kathleen.capella@arm.com>
    Change-Id: Ic4e6dfde7a7bc5b5e7da365afac441382e1f7519

--- src/arch/aarch64/hftest/stdout.c
+++ src/arch/aarch64/hftest/stdout.c
@@ -10,7 +10,35 @@
 
 #include "vmapi/hf/call.h"
 
+#define FFA_CONSOLE_LOG64_MAX_CHAR ((size_t)48)
+#define STDOUT_BUFFER_SIZE ((size_t)4096)
+
+static char stdout_buffer[STDOUT_BUFFER_SIZE];
+
 void stdout_putchar(char c)
 {
-	ffa_console_log_32((const char *)&c, 1);
+	static size_t buffer_head = 0;
+	static size_t buffer_tail = 0;
+	size_t to_write;
+
+	/* Write single char to buffer. */
+	stdout_buffer[buffer_tail++] = c;
+	to_write = buffer_tail - buffer_head;
+
+	/*
+	 * Flush buffer to stdout when buffer is full, a terminal character is
+	 * reached ('\0' or '\n'), or enough characters have been buffered to
+	 * fill all the registers in ffa_console_log_64.
+	 */
+	if (buffer_tail == STDOUT_BUFFER_SIZE) {
+		ffa_console_log_64((const char *)&stdout_buffer[buffer_head],
+				   to_write);
+		buffer_head = 0;
+		buffer_tail = 0;
+	} else if (to_write >= FFA_CONSOLE_LOG64_MAX_CHAR || c == '\0' ||
+		   c == '\n') {
+		ffa_console_log_64((const char *)&stdout_buffer[buffer_head],
+				   to_write);
+		buffer_head = buffer_tail;
+	}
 }
