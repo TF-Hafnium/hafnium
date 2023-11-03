a154d5c7cdfed0711441b64ec31ca6d2a12048e0
Olivier Deprez <olivier.deprez@arm.com>
2023-11-03 17:55:57 +0100

    Revert "fix(fvp): allow SPMC boot without GIC eSPI support"
    This change [1] has created an issue as spotted here [2]
    This reverts commit b7eb20541bf0fc6a50a97697b4742b719c3cff94.
    
    [1] https://review.trustedfirmware.org/c/hafnium/hafnium/+/24275
    [2] https://review.trustedfirmware.org/c/hafnium/hafnium/+/24275/1/src/arch/aarch64/plat/interrupts/gicv3.c#513
    
    Change-Id: Ibc7120a03821ee8fd71dd9486e1bc61476102f57
    Signed-off-by: Olivier Deprez <olivier.deprez@arm.com>

--- src/arch/aarch64/plat/interrupts/gicv3.c
+++ src/arch/aarch64/plat/interrupts/gicv3.c
@@ -448,7 +448,7 @@ void gicv3_send_sgi(uint32_t sgi_id, bool send_to_all, uint64_t mpidr_target,
 /*******************************************************************************
  * Helper function to get the maximum ESPI INTID + 1.
  ******************************************************************************/
-static unsigned int gicv3_get_espi_limit(uintptr_t gicd_base)
+unsigned int gicv3_get_espi_limit(uintptr_t gicd_base)
 {
 	unsigned int typer_reg = read_gicd_typer_reg(gicd_base);
 
@@ -510,16 +510,10 @@ bool gicv3_driver_init(struct mm_stage1_locked stage1_locked,
 	populate_redist_base_addrs();
 
 #if GIC_EXT_INTID
-	if ((read_gicd_typer_reg(plat_gicv3_driver.dist_base) & TYPER_ESPI) !=
-	    TYPER_ESPI) {
-		dlog_warning(
-			"GIC extended SPI support built in but hardware "
-			"doesn't support it.\n");
-	} else {
-		CHECK(gicv3_get_espi_limit(plat_gicv3_driver.dist_base) != 0);
-	}
+	CHECK((read_gicd_typer_reg(plat_gicv3_driver.dist_base) & TYPER_ESPI) ==
+	      TYPER_ESPI);
+	CHECK(gicv3_get_espi_limit(plat_gicv3_driver.dist_base) != 0);
 #endif
-
 	return true;
 }
 
