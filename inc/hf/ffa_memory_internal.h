95f0e6033ad954b84ad224a96dc6256d0be34512
Karl Meakin <karl.meakin@arm.com>
2024-03-07 17:23:26 +0000

    fix: fix typo in static_assert
    The assertion `static_assert(sizeof(((struct
    ffa_memory_region){0}).receiver_count == 4))` was intended to compare
    the size of the `receiver_count` field against 4. But due to a typo, it
    actually compares the *value* of the `receiver_count` field against 0.
    `sizeof` then returns the size of the type of that expression (ie
    `sizeof(typeof(0 == 4))` => `sizeof(int)`), which is guaranteed to be
    nonzero. In other words, this static_assert will always be true,
    regardless of the size of the `receiver_count` field, so a breaking
    change would silently pass undetected.
    
    This patch fixes the assertion to correctly compare the size of the
    `receiver_count` field against 4.
    
    Change-Id: Ieb08629b4ac5c14cffed1d0d88853039db552b91
    Signed-off-by: Karl Meakin <karl.meakin@arm.com>

--- inc/hf/ffa_memory_internal.h
+++ inc/hf/ffa_memory_internal.h
@@ -47,7 +47,7 @@ static_assert(sizeof(struct ffa_memory_region) % 16 == 0,
 static_assert(sizeof(struct ffa_mem_relinquish) % 16 == 0,
 	      "struct ffa_mem_relinquish must be a multiple of 16 "
 	      "bytes long.");
-static_assert(sizeof(((struct ffa_memory_region){0}).receiver_count == 4),
+static_assert(sizeof((struct ffa_memory_region){0}.receiver_count) == 4,
 	      "struct ffa_memory_region::receiver_count must be 4 bytes long");
 
 static_assert(sizeof(struct ffa_features_rxtx_map_params) == 4,
