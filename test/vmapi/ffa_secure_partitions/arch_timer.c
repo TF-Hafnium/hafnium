65827d7035351bfe39985851d23908b3e34da989
Madhukar Pappireddy <madhukar.pappireddy@arm.com>
2024-09-25 20:47:30 -0500

    test: migrate vCPU of SP with pending timer deadline
    This test causes the vCPU of an SP with pending timer to be migrated to
    a different CPU.
    
    Change-Id: Id8b806e71334812093266c0167424223382200a4
    Signed-off-by: Madhukar Pappireddy <madhukar.pappireddy@arm.com>

--- test/vmapi/ffa_secure_partitions/arch_timer.c
+++ test/vmapi/ffa_secure_partitions/arch_timer.c
@@ -18,10 +18,20 @@
 #include "sp805.h"
 #include "sp_helpers.h"
 
+#define SP_SKIP_SLEEP 0U
 #define SP_SLEEP_TIME 100U
 #define SP_SHORT_SLEEP 10U
+#define TIMER_DEADLINE 50U
 
 #define FORWARD_TIMER_CMD 1
+
+#define LAST_SECONDARY_VCPU_ID (MAX_CPUS - 1)
+
+struct secondary_cpu_entry_args {
+	ffa_id_t receiver_id;
+	ffa_vcpu_index_t vcpu_id;
+	struct spinlock lock;
+};
 
 /**
  * This test requests SP to start the timer with short deadline and wait for
@@ -256,4 +266,115 @@ TEST(arch_timer, multiple_sp_deadline)
 
 	check_arch_timer_virtual_interrupt_serviced_all_sp(
 		own_id, receiver1_id, receiver2_id, receiver3_id);
+}
+
+static void migrate_up_sp_pending_timer(uintptr_t arg)
+{
+	ffa_id_t own_id = hf_vm_get_id();
+	struct ffa_value res;
+
+	struct secondary_cpu_entry_args *args =
+		// NOLINTNEXTLINE(performance-no-int-to-ptr)
+		(struct secondary_cpu_entry_args *)arg;
+
+	if (args->vcpu_id == LAST_SECONDARY_VCPU_ID) {
+		/* Migrate the vCPU of SP with pending timer to CPU7. */
+		res = sp_sleep_cmd_send(own_id, args->receiver_id,
+					SP_SLEEP_TIME, 0);
+
+		/*
+		 * Timer deadline would have expired during this time. SP will
+		 * handle the virtual timer interrupt.
+		 */
+		EXPECT_EQ(res.func, FFA_MSG_SEND_DIRECT_RESP_32);
+
+		/* Make sure elapsed time not less than sleep time. */
+		EXPECT_GE(sp_resp_value(res), SP_SLEEP_TIME);
+
+		/* Check for the last serviced secure virtual interrupt. */
+		res = sp_get_last_interrupt_cmd_send(own_id, args->receiver_id);
+
+		EXPECT_EQ(res.func, FFA_MSG_SEND_DIRECT_RESP_32);
+		EXPECT_EQ(sp_resp(res), SP_SUCCESS);
+
+		/* Make sure virtual timer interrupt was serviced. */
+		EXPECT_EQ(sp_resp_value(res), HF_VIRTUAL_TIMER_INTID);
+	}
+
+	/* Releases the lock passed in. */
+	sl_unlock(&args->lock);
+	arch_cpu_stop();
+}
+
+/**
+ * This test causes the vCPU of an SP with pending timer to be migrated to a
+ * different CPU.
+ */
+TEST_PRECONDITION_LONG_RUNNING(arch_timer, migrate_with_deadline,
+			       service2_is_up_sp)
+{
+	struct ffa_value res;
+	ffa_id_t own_id = hf_vm_get_id();
+	struct secondary_cpu_entry_args args;
+	struct mailbox_buffers mb = set_up_mailbox();
+	struct ffa_partition_info *up_receiver_info = service2(mb.recv);
+	ffa_id_t up_receiver_id = up_receiver_info->vm_id;
+	struct ffa_partition_info *companion_info = service3(mb.recv);
+	ffa_id_t companion_id = companion_info->vm_id;
+
+	ASSERT_EQ(up_receiver_info->vcpu_count, 1);
+
+	args.receiver_id = up_receiver_id;
+	res = sp_virtual_interrupt_cmd_send(own_id, up_receiver_id,
+					    HF_VIRTUAL_TIMER_INTID, true, 0);
+
+	EXPECT_EQ(res.func, FFA_MSG_SEND_DIRECT_RESP_32);
+	EXPECT_EQ(sp_resp(res), SP_SUCCESS);
+
+	res = sp_virtual_interrupt_cmd_send(own_id, companion_id,
+					    HF_VIRTUAL_TIMER_INTID, true, 0);
+
+	EXPECT_EQ(res.func, FFA_MSG_SEND_DIRECT_RESP_32);
+	EXPECT_EQ(sp_resp(res), SP_SUCCESS);
+
+	res = sp_program_arch_timer_sleep_cmd_send(own_id, companion_id, 30,
+						   SP_SKIP_SLEEP, 0);
+	EXPECT_EQ(res.func, FFA_MSG_SEND_DIRECT_RESP_32);
+	EXPECT_EQ(sp_resp(res), SP_SUCCESS);
+
+	res = sp_program_arch_timer_sleep_cmd_send(
+		own_id, up_receiver_id, TIMER_DEADLINE, SP_SKIP_SLEEP, 0);
+
+	EXPECT_EQ(res.func, FFA_MSG_SEND_DIRECT_RESP_32);
+	EXPECT_EQ(sp_resp(res), SP_SUCCESS);
+
+	/* Start secondary EC while holding lock. */
+	sl_lock(&args.lock);
+
+	for (size_t i = 1; i < MAX_CPUS; i++) {
+		uintptr_t id;
+
+		id = hftest_get_cpu_id(i);
+		args.vcpu_id = i;
+		HFTEST_LOG("Booting CPU %zu - %lx", i, id);
+
+		EXPECT_EQ(hftest_cpu_start(id, hftest_get_secondary_ec_stack(i),
+					   migrate_up_sp_pending_timer,
+					   (uintptr_t)&args),
+			  true);
+
+		/* Wait for CPU to release the lock. */
+		sl_lock(&args.lock);
+
+		HFTEST_LOG("Done with CPU %zu", i);
+	}
+
+	/* Check for the last serviced secure virtual interrupt. */
+	res = sp_get_last_interrupt_cmd_send(own_id, companion_id);
+
+	EXPECT_EQ(res.func, FFA_MSG_SEND_DIRECT_RESP_32);
+	EXPECT_EQ(sp_resp(res), SP_SUCCESS);
+
+	/* Make sure virtual timer interrupt was serviced. */
+	EXPECT_EQ(sp_resp_value(res), HF_VIRTUAL_TIMER_INTID);
 }
