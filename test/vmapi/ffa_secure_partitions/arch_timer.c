fe10878b1e1c75923e0a4223a6f2e4ed2fd1e959
Madhukar Pappireddy <madhukar.pappireddy@arm.com>
2024-09-25 20:13:47 -0500

    test: SP reprograms the arch timer deadline
    This test requests SP to program the timer with a long deadline and then
    requests again to program with a short deadline. SPMC shall honor the
    most recent configuration of the arch timer by the SP.
    
    Change-Id: I25ad62a7932660cdb9dcf12ea34466cc53bdbcdf
    Signed-off-by: Madhukar Pappireddy <madhukar.pappireddy@arm.com>

--- test/vmapi/ffa_secure_partitions/arch_timer.c
+++ test/vmapi/ffa_secure_partitions/arch_timer.c
@@ -63,3 +63,52 @@ TEST(arch_timer, short_deadline)
 	/* Make sure virtual timer interrupt was serviced. */
 	EXPECT_EQ(sp_resp_value(res), HF_VIRTUAL_TIMER_INTID);
 }
+
+/**
+ * This test requests SP to program the timer with a long deadline and then
+ * requests again to program with a short deadline. SPMC shall honor the most
+ * recent configuration of the arch timer by the SP.
+ */
+TEST(arch_timer, reprogram_deadline)
+{
+	struct ffa_value res;
+	ffa_id_t own_id = hf_vm_get_id();
+	struct mailbox_buffers mb = set_up_mailbox();
+	struct ffa_partition_info *service2_info = service2(mb.recv);
+	const ffa_id_t receiver_id = service2_info->vm_id;
+
+	res = sp_virtual_interrupt_cmd_send(own_id, receiver_id,
+					    HF_VIRTUAL_TIMER_INTID, true, 0);
+
+	EXPECT_EQ(res.func, FFA_MSG_SEND_DIRECT_RESP_32);
+	EXPECT_EQ(sp_resp(res), SP_SUCCESS);
+
+	res = sp_program_arch_timer_sleep_cmd_send(own_id, receiver_id, 100,
+						   SP_SHORT_SLEEP, 0);
+	EXPECT_EQ(res.func, FFA_MSG_SEND_DIRECT_RESP_32);
+	EXPECT_EQ(sp_resp(res), SP_SUCCESS);
+
+	res = sp_program_arch_timer_sleep_cmd_send(own_id, receiver_id, 5,
+						   SP_SHORT_SLEEP, 0);
+	EXPECT_EQ(res.func, FFA_MSG_SEND_DIRECT_RESP_32);
+	EXPECT_EQ(sp_resp(res), SP_SUCCESS);
+
+	/*
+	 * Timer deadline would have expired during this time. SP will handle
+	 * the virtual timer interrupt.
+	 */
+	EXPECT_EQ(res.func, FFA_MSG_SEND_DIRECT_RESP_32);
+	EXPECT_EQ(sp_resp(res), SP_SUCCESS);
+
+	/* Make sure elapsed time not less than sleep time. */
+	EXPECT_GE(sp_resp_value(res), SP_SHORT_SLEEP);
+
+	/* Check for the last serviced secure virtual interrupt. */
+	res = sp_get_last_interrupt_cmd_send(own_id, receiver_id);
+
+	EXPECT_EQ(res.func, FFA_MSG_SEND_DIRECT_RESP_32);
+	EXPECT_EQ(sp_resp(res), SP_SUCCESS);
+
+	/* Make sure virtual timer interrupt was serviced. */
+	EXPECT_EQ(sp_resp_value(res), HF_VIRTUAL_TIMER_INTID);
+}
