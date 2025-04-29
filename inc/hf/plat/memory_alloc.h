f93e9615ec118bb6e2181c83af625feec9158058
Kathleen Capella <kathleen.capella@arm.com>
2025-04-29 12:28:45 -0500

    feat(plat): define memory alloc library
    Add APIs that allow specification of a platform-specific memory
    allocation library.
    Provide the default implementation based on mpool, and absent
    for partition targets.
    
    Signed-off-by: J-Alves <joao.alves@arm.com>
    Signed-off-by: Kathleen Capella <kathleen.capella@arm.com>
    Change-Id: I5a30c24a292caaa27fef5a53bb57c7aa0c82808b

---
+++ inc/hf/plat/memory_alloc.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2025 The Hafnium Authors.
+ *
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/BSD-3-Clause.
+ */
+
+#include "hf/arch/std.h"
+
+void memory_alloc_init(void);
+void *memory_alloc(size_t size);
+void memory_free(void *begin, size_t size);
